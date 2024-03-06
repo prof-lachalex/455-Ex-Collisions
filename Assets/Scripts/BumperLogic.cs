0c459ab53ea445ca9f9e3fed1d11cac2d5b0a7b5
AL-CAL <alexandre.lachapelle@claurendeau.qc.ca>
2024-03-06 16:17:54 -0500

    Ajout de la collision entre Bumper et GameBall, on garde la valeur de force hardcodée pour l'instant

--- Assets/Scripts/BumperLogic.cs
+++ Assets/Scripts/BumperLogic.cs
@@ -18,6 +18,18 @@ public class BumperLogic : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (collision == null || collision.gameObject == null || collision.contactCount <= 0) 
+            return;
 
+        GameObject obj = collision.gameObject;
+        if (obj.CompareTag("GameBall"))
+        {
+            // Rigidbody de la balle
+            Rigidbody rb = obj.GetComponent<Rigidbody>();
+            if(rb != null) 
+            {
+                rb.AddForce(-collision.GetContact(0).normal * 15f, ForceMode.Impulse);
+            }
+        }
     }
 }
